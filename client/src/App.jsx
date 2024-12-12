204101a9e8ab01d6160fc83902ae6473f3cd7922
kevin kalenga <kevinkalenga10@gmail.com>
2024-12-12 09:41:35 +0100

    Install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+  return (
+    <div className="text-white bg-slate-800 p-5 text-center font-semibold hover:text-red-600 cursor-pointer">
+      App
+    </div>
+  )
+}
