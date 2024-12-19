e5ef5cc1883b942b1658c9cc0a66d527dce19711
kevin kalenga <kevinkalenga10@gmail.com>
2024-12-19 09:44:22 +0100

    Create the Header component

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,28 @@
+/*
 
+<Navbar.Collapse>
+           <Navbar.Link to="/">
+                 <Link to="/">
+                    Home
+                 </Link>
+           </Navbar.Link>
+           <Navbar.Link>
+                 <Link to="/about">
+                    About
+                 </Link>
+           </Navbar.Link>
+           <Navbar.Link>
+                 <Link to="/projects">
+                    Projects
+                 </Link>
+           </Navbar.Link>
+      </Navbar.Collapse>
+   </Navbar>
+   
+
+
+
+*/ 
 
 export default function About() {
   return (
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,97 @@
+
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',     
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
+};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     './index.html',
+//     './src/**/*.{js,ts,jsx,tsx}',
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('flowbite/plugin'),
+  
+//   ],
+// };
+
+
+// const flowbite = require("flowbite-react/tailwind");
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// module.exports = {
+//   content: [
+//     // ...
+//     flowbite.content(),
+//   ],
+//   plugins: [
+//     // ...
+//     flowbite.plugin(),
+//   ],
+// };
+
+//////////////////////////////Chatgpt////////////////////////////////////////////
+
+
+// const withMT = require("@material-tailwind/react/utils/withMT"); // Facultatif si vous utilisez Material Tailwind avec Flowbite
+
+// module.exports = withMT({
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Vos fichiers React
+//     "./node_modules/flowbite-react/**/*.js", // Les composants Flowbite React
+//     "./node_modules/flowbite/**/*.js", // Les utilitaires de Flowbite
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Exemple d'extension des couleurs personnalisées
+//         primary: {
+//           light: "#4caf50",
+//           DEFAULT: "#388e3c",
+//           dark: "#1b5e20",
+//         },
+//       },
+//       fontFamily: {
+//         // Ajout de polices personnalisées si nécessaire
+//         sans: ["Inter", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [
+//     require("flowbite/plugin"), // Intégration des plugins Flowbite
+//   ],
+// });
+
+
+
+
+
+
+
+
+ 
