985472e81b8d37df78dfae60a051e4266f2c759c
kevin kalenga <kevinkalenga10@gmail.com>
2024-12-21 02:55:24 +0100

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+
+// when we have more than one reducer we have to combine them
+const rootReducer = combineReducers({
+  user:userReducer
+});
+
+const persistConfig = {
+   key: 'root',
+   storage,
+   version:1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 // use that in the signin page
 export const store = configureStore({
-  reducer: {
-    user:userReducer,
-  },
+  // instead of having alot of reducer we can just have persistedReducer 
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({serializableCheck: false}),
+  
 })
+// export the store that is persisted
+export const persistor = persistStore(store);
