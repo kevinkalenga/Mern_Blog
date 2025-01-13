2bedaf4babc5e8995c9ee8ef37f620871f7412a9
kevin kalenga <kevinkalenga10@gmail.com>
2025-01-13 02:18:04 +0100

    Add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,10 +1,11 @@
 
 import {useSelector} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import {Alert, Button, Textarea} from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import Comment from './Comment';
 export default function CommentSection({postId}) {
+  const navigate = useNavigate()
   const {currentUser} = useSelector(state => state.user);
   const [commentError, setCommentError] = useState(null);
   const [comment, setComment] = useState('');
@@ -56,6 +57,34 @@ export default function CommentSection({postId}) {
   
   }, [postId])
   
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in');
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT',
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -126,7 +155,7 @@ export default function CommentSection({postId}) {
                 <Comment 
                 key={comment._id}
                 comment={comment}
-
+                 onLike={handleLike}
                 />
               ))
             }
