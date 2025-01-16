577dd9716c38d3d11e2f5732a34a04564b037166
kevin kalenga <kevinkalenga10@gmail.com>
2025-01-16 06:38:27 +0100

    Add dashboard overview to the admin dashboard(2)

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import {Sidebar} from 'flowbite-react';
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup,  HiAnnotation, HiUser} from 'react-icons/hi';
+import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup,  HiAnnotation, HiUser,HiChartPie} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux';
@@ -43,7 +43,19 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
        <Sidebar.Items>
          <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            <Link to='/dashboard?tab=profile'>
+         {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+            
+          <Link to='/dashboard?tab=profile'>
               <Sidebar.Item 
                  active={tab === 'profile'} 
                  icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashComponent from '../components/DashboadComponent';
 
 export default function Dashboard() {
   
@@ -36,6 +37,8 @@ export default function Dashboard() {
        {tab === 'users' && <DashUsers />}
        {/* comments */}
        {tab === 'comments' && <DashComments />}
+       {/* dashboard comp */}
+       {tab === 'dash' && <DashComponent /> }
     </div>
    
   )
